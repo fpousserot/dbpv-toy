6c7bebf0f09658f90938ea51eb54d215641255d0
lukovnikov <lukovnikov@outlook.com>
2013-09-10 17:44:40 +0200

    qsdf

--- js/dbpv.js
+++ js/dbpv.js
@@ -1,5 +1,5 @@
 dbpv.config(function($routeProvider, $locationProvider) {
-	//$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);
 	$routeProvider
 		.when('/page/:id', {templateUrl: '/tpl/entity.html', controller: EntityCtrl})
 		.when('/resource/:id', {redirectTo: function(params, a, search) {return '/page/'+params.id;} })
